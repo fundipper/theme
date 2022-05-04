3f05b9a8e578b66ac1a81a3987f21db5ee05a714
andy.jiang <150haomengting@163.com>
2022-05-04 22:18:41 +0800

    upgrade to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,43 @@
 module.exports = {
-  content: ["./templates/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-}
+	content: ["./templates/*.html"],
+	theme: {
+		extend: {
+			animation: {
+				"pulsate-fwd": "pulsate-fwd 0.5s ease  infinite both",
+				"fade-in-bl": "fade-in-bl 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+				"fade-in-br": "fade-in-br 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+			},
+			keyframes: {
+				"pulsate-fwd": {
+					"0%,to": {
+						transform: "scale(1)",
+					},
+					"50%": {
+						transform: "scale(1.1)",
+					},
+				},
+				"fade-in-bl": {
+					"0%": {
+						transform: "translateX(-50px) translateY(50px)",
+						opacity: "0",
+					},
+					to: {
+						transform: "translateX(0) translateY(0)",
+						opacity: "1",
+					},
+				},
+				"fade-in-br": {
+					"0%": {
+						transform: "translateX(50px) translateY(50px)",
+						opacity: "0",
+					},
+					to: {
+						transform: "translateX(0) translateY(0)",
+						opacity: "1",
+					},
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
+};
